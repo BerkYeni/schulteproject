d2da72d14c0b3e217ff754ba733ac5079fe54dce
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2024-11-08 17:28:45 +0300

    abstracted hide panels button to its own component

---
+++ src/components/HidePanelsButton.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import { OnExposePanels, OnHidePanels } from "../interfaces";
+
+interface HidePanelsButtonProps {
+  hidden: boolean;
+  onHidePanels: OnHidePanels;
+  onExposePanels: OnExposePanels;
+}
+
+const HidePanelsButton: FC<HidePanelsButtonProps> = (props) => {
+  // possibly add a custom hook for controlling hide/expose logic
+  const { hidden, onHidePanels, onExposePanels } = props;
+
+  return (
+    <>
+      <button
+        id="exposePanelsButton"
+        // className={`exposePanels tile unclicked ${!hidden && "hidden"}`}
+        className={`exposePanels tile unclicked`}
+        onClick={hidden ? onExposePanels : onHidePanels}
+      >
+        →
+      </button>
+
+      {/* <div className="toggleVisibilityContainer">
+        <button
+          className="toggleVisibility tile unclicked"
+          onClick={onHidePanels}
+        >
+          ←
+        </button>
+      </div> */}
+    </>
+  )
+}
+
+
+export default HidePanelsButton;
