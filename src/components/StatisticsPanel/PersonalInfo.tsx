9510a27a4c11361e55f6ee725ac75d74c8071e6b
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2024-11-10 16:51:14 +0300

    created personal info comp

---
+++ src/components/StatisticsPanel/PersonalInfo.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+
+interface PersonalInfoProps {
+}
+
+const PersonalInfo: FC<PersonalInfoProps> = (props) => {
+  // const {  } = props;
+
+  return (
+    <div>
+      See source code
+    </div>
+  );
+};
+
+export default PersonalInfo;
