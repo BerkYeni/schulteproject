3e84337f2c470bcbbf2e4bbeb01e8118d472b6f6
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2024-11-10 18:06:31 +0300

    completed info comp

--- src/components/StatisticsPanel/PersonalInfo.tsx
+++ src/components/StatisticsPanel/PersonalInfo.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 // import { ReactComponent as GithubMark } from "../../github-mark/github-mark-white.svg";
-import githubMark from "../../github-mark/github-mark-white.png"
+import githubMark from "../../github-mark/github-mark-white.png";
+import berkyeniMark from "../../berkyeni-mark.png";
+
 
 interface PersonalInfoProps {
 }
@@ -11,15 +13,15 @@ const PersonalInfo: FC<PersonalInfoProps> = (props) => {
   return (
     <div className="personalInfo">
 
-      <a href="https://github.com/BerkYeni/schulteproject" className="infoLink" target="_blank" rel="noopener noreferrer">
-        <div className="github">
-          <span>See the source code</span><img className="githubMark" src={githubMark} alt="GitHub Mark" />
+      <a href="https://berkyeni.com" className="infoLink" target="_blank" rel="noopener noreferrer">
+        <div className="siteLinkInner">
+          <span>Visit my website</span><img className="siteMark" src={berkyeniMark} alt="Berk Yeni's Mark" />
         </div>
       </a>
 
-      <a href="https://berkyeni.com" className="infoLink" target="_blank" rel="noopener noreferrer">
-        <div className="github">
-          <span>Visit my website</span><img className="githubMark" src={githubMark} alt="Berk Yeni's Mark" />
+      <a href="https://github.com/BerkYeni/schulteproject" className="infoLink" target="_blank" rel="noopener noreferrer">
+        <div className="siteLinkInner">
+          <span>See the source code</span><img className="siteMark" src={githubMark} alt="GitHub Mark" />
         </div>
       </a>
 
