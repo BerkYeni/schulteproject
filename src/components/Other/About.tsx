f2da7ba472f084b4f3e6dd60be8675b52bbb0860
BerkYeni <141870638+BerkYeni@users.noreply.github.com>
2024-11-15 19:06:34 +0300

    removed unnecessary commented code again

--- src/components/Other/About.tsx
+++ src/components/Other/About.tsx
@@ -5,13 +5,6 @@ interface AboutProps {
 }
 
 const About: FC<AboutProps> = (props) => {
-  // const sectionContent = () => (
-  //   <span>This CodePen demonstrates how we can animate the opening and closing
-  //   states of an accordion with fancy reveal animation using only CSS. This
-  //   concept is suitable for creating <strong>FAQ sections</strong>,
-  //   <strong>Table of Contents</strong>, and more.</span>
-  // )
-
   const aboutSections = [
     {
       title: "🧩 About This Website",
